93195d2d88789d0c6585b84ca856fe46c5909730
vkostenko <git@github.com:TheViktorSAy/Hellow-World.git>
2025-02-28 20:41:38 +0300

    Initial

---
+++ src/main/java/ru/hogwarts/demoschool/DemoschoolApplication.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.demoschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoschoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoschoolApplication.class, args);
+	}
+
+}
